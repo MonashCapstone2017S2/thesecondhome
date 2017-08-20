616096f5901a9b79d6e1396e0b7d9c59a6369250
yang deng <fredrickdeng@hotmail.com>
2017-08-20 21:00:33 +1000

    Week4Finish2
    Combine Yang and Roshan

--- JS/quiz.js
+++ JS/quiz.js
@@ -2,16 +2,10 @@ Survey.Survey.cssType = "bootstrap";
 Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
 
 window.survey = new Survey.Model( { questions: [
-    {name:"name", type:"text", title: "Please enter your name:", placeHolder:"Jon Snow", isRequired: true},
-    {name:"birthdate", type:"text", inputType:"date", title: "Your birthdate:", isRequired: true},
-    {name:"email", type:"text", inputType:"email", title: "Your e-mail:", placeHolder:"jon.snow@nightwatch.org", isRequired: true, validators: [{type:"email"}]},
-    {name:"kidName", type:"text", title: "Please enter the name of the child", isRequired: true},
     { type: "radiogroup", name: "kidGender", title: "Please select the gender of the child", isRequired: true,
         colCount: 4, choices: ["Boy","Girl"]},
     { type: "dropdown", name: "Age", title: "What is the age of the child?", isRequired: true, colCount: 0,
         choices: ["0-6", "6-10", "10-14", "14-18"] },
-    { type: "dropdown", name: "Relationship", title: "What is the relationship between you and the child?", isRequired: true, colCount: 0,
-        choices: ["Neighborhood", "Friends", "Church Member", "Teacher", "Distant Relative", "Others"] },
     { type: "dropdown", name: "Friends", title: "How many friends do the child have?", isRequired: false, colCount: 0,
         choices: ["Few Friend", "One or Two Friends", "Many Friends", "Not Sure"] },
     { type: "checkbox", name: "Sports", title: "Have you noticed that the child engaged in any sports?", isRequired: false,
@@ -23,8 +17,45 @@ window.survey = new Survey.Model( { questions: [
     { type: "comment", name: "SpecialSituation", title:"Have you noticed some special situation for this kids?(For Example: Domestic Violence)" }
 ]});
 survey.onComplete.add(function(result) {
-    document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(result.data);
+
+    var emotion = result.data['Emotion'];
+    var socialEvents = result.data['SocialEvents'];
+    var sports = result.data['Sports'];
+    var friends = result.data ['Friends'];
+    var age = result.data ['Age'];
+    var gender = result.data['kidGender'];
+
+
+
+    var $alert1  = $("#alert1");
+    var $alert2 =  $("#alert2");
+    var $alertSad = $("#alert-sad");
+    var $alertHappy = $("#alert-happy");
+    var $alertOK   = $("#ok");
+
+
+
+    if(parseInt(emotion) <=2 && (friends == "Few Friend" || friends == "Not Sure" || friends == "One or Two Friends") ){
+
+        $alertSad.show();
+
+        $alert1.stop().fadeIn(300);
+        //$alertSad.css("visibility","hidden");
+    }
+    else{
+        $alertHappy.show();
+        $alert2.stop().fadeIn(300);
+        //$alertHappy.css("visibility","hidden");
+
+    }
+
+    $(".ok").click(function(){
+        $("#alertSad").hide();
+        $("$alertHappy").hide();
+    });
+
 });
+
 
 
 $("#surveyElement").Survey({model:survey});
